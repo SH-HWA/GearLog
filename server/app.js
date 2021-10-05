5a933a8c6aaa264fc3ec431ba1ec6418526a2bcd
hi33741 <hi33741@naver.com>
2021-10-05 16:00:45 +0900

    first setting

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  res.send("헬로월드다");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 에서 스타트중");
+});
