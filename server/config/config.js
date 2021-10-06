27c7aecb1ade32d2bd33e3f52ea02a92309ffc79
Hyun H <raidi9069@gmail.com>
2021-10-06 16:42:04 +0900

    10/06/16:40

--- server/config/config.js
+++ server/config/config.js
@@ -7,21 +7,24 @@ const config = {
     password: process.env.DATABASE_PASSWORD,
     database: "gearlog",
     host: process.env.DATABASE_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    port: process.env.DATABASE_PORT
   },
   test: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: "gearlog",
     host: process.env.DATABASE_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    port: process.env.DATABASE_PORT
   },
   production: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: "gearlog",
     host: process.env.DATABASE_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    port: process.env.DATABASE_PORT
   }
 }
 
