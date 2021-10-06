8d086aa40cc175bf84d0f0f9d37e7176e10570cf
Hyun H <raidi9069@gmail.com>
2021-10-06 15:57:33 +0900

    10/06/15:58

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 require('dotenv').config(); 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 
--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,28 @@
-require('dotenv').config(); 
+const dotenv = require('dotenv');
+dotenv.config();
 
-module.exports = {
-  "development": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": "gearlog",
-    "host": process.env.DATABASE_HOST,
-    "dialect": "mysql"
+const config = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: "gearlog",
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql"
   },
-  "test": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": "gearlog",
-    "host": process.env.DATABASE_HOST,
-    "dialect": "mysql"
+  test: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: "gearlog",
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql"
   },
-  "production": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": "gearlog",
-    "host": process.env.DATABASE_HOST,
-    "dialect": "mysql"
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: "gearlog",
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql"
   }
 }
+
+module.exports = config;
