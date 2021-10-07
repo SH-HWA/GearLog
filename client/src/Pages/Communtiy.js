9fac43588d3f70ff34e76e146fe288de17a716fd
hi33741 <hi33741@naver.com>
2021-10-08 00:14:09 +0900

    board 업데이트 설정완료 글쓰기 분리와 게시판 성격 중고와 자유게시판 만들어야함

--- client/src/Pages/Communtiy.js
+++ client/src/Pages/Communtiy.js
@@ -2,64 +2,63 @@ import { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ReactHtmlParser from 'react-html-parser';
-import axios from 'axios';
+import Axios from 'axios';
+import './Communtiy.css';
 
 function Communtiy() {
   const [movieContent, setMovieContent] = useState({
     title: '',
-    content: ''
-  })
+    content: '',
+  });
 
   const [viewContent, setViewContent] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:3000/api/get').then((response)=>{
-      setViewContent(response.data);
-    })
-  },[viewContent])
+  // useEffect(() => {
+  //   Axios.get('http://localhost:8000/api/get').then((response) => {
+  //     setViewContent(response.data);
+  //   });
+  // }, [viewContent]);
 
-  const submitReview = ()=>{
-    axios.post('http://localhost:3000/api/insert', {
-      title: movieContent.title,
-      content: movieContent.content
-    }).then(()=>{
-      alert('등록 완료!');
-    })
-  };
+  // const submitReview = () => {
+  //   Axios.post('http://localhost:8000/api/insert', {
+  //     title: movieContent.title,
+  //     content: movieContent.content,
+  //   }).then(() => {
+  //     alert('등록 완료!');
+  //   });
+  // };
 
-  const getValue = e => {
+  const getValue = (e) => {
     const { name, value } = e.target;
     setMovieContent({
       ...movieContent,
-      [name]: value
-    })
+      [name]: value,
+    });
   };
 
-
   return (
-    <div className="Communtiy">
-      <h1>jangbi Review</h1>
-      <div className='movie-container'>
-        {viewContent.map(element =>
+    <div className="App">
+      <h1>Movie Review</h1>
+      <div className="movie-container">
+        {viewContent.map((element) => (
           <div style={{ border: '1px solid #333' }}>
             <h2>{element.title}</h2>
-            <div>
-              {ReactHtmlParser(element.content)}
-            </div>
+            <div>{ReactHtmlParser(element.content)}</div>
           </div>
-        )}
+        ))}
       </div>
-      <div className='form-wrapper'>
-        <input className="title-input"
-          type='text'
-          placeholder='제목'
+      <div className="form-wrapper">
+        <input
+          className="title-input"
+          type="text"
+          placeholder="제목"
           onChange={getValue}
-          name='title'
+          name="title"
         />
         <CKEditor
           editor={ClassicEditor}
           data="<p>Hello from CKEditor 5!</p>"
-          onReady={editor => {
+          onReady={(editor) => {
             // You can store the "editor" and use when it is needed.
             console.log('Editor is ready to use!', editor);
           }}
@@ -68,8 +67,8 @@ function Communtiy() {
             console.log({ event, editor, data });
             setMovieContent({
               ...movieContent,
-              content: data
-            })
+              content: data,
+            });
           }}
           onBlur={(event, editor) => {
             console.log('Blur.', editor);
@@ -81,8 +80,12 @@ function Communtiy() {
       </div>
       <button
         className="submit-button"
-        onClick={submitReview}
-        >입력</button>
+        onClick={() => {
+          setViewContent(viewContent.concat({ ...movieContent }));
+        }}
+      >
+        입력
+      </button>
     </div>
   );
 }
