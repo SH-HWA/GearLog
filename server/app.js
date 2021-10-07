d1e4a5dfd82753f0b79c15f69a4a50d2829a9b73
seismman <seismman@gmail.com>
2021-10-07 17:26:26 +0900

    making router

--- server/app.js
+++ server/app.js
@@ -1,16 +1,21 @@
 require('dotenv').config(); 
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
 const express = require("express");
 const app = express();
-const cors = require("cors");
+
+const controllers = require('./controllers');
 
 app.use(cors());
-
-app.set("port", process.env.PORT || 3000);
+app.use(cookieParser());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("헬로월드다");
 });
 
+app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 에서 스타트중");
 });
+
