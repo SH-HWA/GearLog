04a7b2b048eaecfde23f536d14641176caaa28e4
hi33741 <hi33741@naver.com>
2021-10-09 17:23:13 +0900

    123123

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,18 +1,19 @@
-const { userinfo } = require('../../models');
-const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
+const { userinfo } = require("../../models");
+const { generateAccessToken, sendAccessToken } = require("../tokenFunctions");
 
 module.exports = async (req, res) => {
+  console.log(req.body);
   const { email, password } = req.body;
   let data = await userinfo.findOne({
     where: {
       email: email,
       password: password,
-    }
-  })
+    },
+  });
   if (!data) {
-    return res.status(404).send('invalid user')
+    return res.status(404).send({ message: "invalid user" });
   }
-    
+
   const accessToken = generateAccessToken(data.dataValues);
 
   sendAccessToken(res, accessToken);
--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -1,32 +1,38 @@
-const { userinfo } = require('../../models');
-const { generateAccessToken } = require('../tokenFunctions');
+const { userinfo } = require("../../models");
+const { generateAccessToken } = require("../tokenFunctions");
 
 module.exports = (req, res) => {
-  const { username, email, password } = req.body;
+  console.log("12312312", req.body);
+  const { email, password } = req.body;
 
-  if (!email || !password || !username) {
-    res.status(422).send('모든 정보는 필수 입력 사항입니다.')
+  if (!email || !password) {
+    res.status(422).send("모든 정보는 필수 입력 사항입니다.");
   }
 
-  userinfo.findOne({
-    where: {
-      email
-    }
-  }).then((data) => {
-    if (data) {
-      return res.status(409).send('이미 가입된 이메일 주소입니다.')
-    } else {
-      userinfo.create(req.body)
-      const accessToken = generateAccessToken(req.body);
+  userinfo
+    .findOne({
+      where: {
+        email,
+      },
+    })
+    .then((data) => {
+      if (data) {
+        return res.status(409).json({
+          message: "이미 가입된 이메일 주소입니다.",
+        });
+      } else {
+        userinfo.create(req.body);
+        const accessToken = generateAccessToken(req.body);
 
-      res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        expiresIn: '180m',
-        sameSite: "Strict",
-        secure: true,
-      }).status(201).json({ message: 'signup ok' })
-    }
-  })
-
-
-}
+        res
+          .cookie("accessToken", accessToken, {
+            httpOnly: true,
+            expiresIn: "180m",
+            sameSite: "Strict",
+            secure: true,
+          })
+          .status(201)
+          .send({ message: "signup ok" });
+      }
+    });
+};
